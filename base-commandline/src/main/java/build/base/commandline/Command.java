8ed597030ca177854271f96fbd3f9d1733208222
Reed von Redwitz <reed+github@vonredwitz.com>
2026-04-08 14:33:17 +0200

    update year

--- base-commandline/src/main/java/build/base/commandline/Command.java
+++ base-commandline/src/main/java/build/base/commandline/Command.java
@@ -4,7 +4,7 @@ package build.base.commandline;
  * #%L
  * base.build Command Line
  * %%
- * Copyright (C) 2025 Workday Inc
+ * Copyright (C) 2026 Workday Inc
  * %%
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
